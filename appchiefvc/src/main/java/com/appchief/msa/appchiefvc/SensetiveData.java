9c43e21dab3f19b8b9b7f35420c32fa970570c3b
mac <iraqrev@gmail.com>
2018-06-23 00:44:47 +0300

    --first commit

---
+++ appchiefvc/src/main/java/com/appchief/msa/appchiefvc/SensetiveData.java
@@ -0,0 +1,13 @@
+package com.appchief.msa.appchiefvc;
+
+/**
+ * Created by mac on 3/9/18.
+ */
+ interface SensetiveData {
+      String token = "dcc919e6d156da91e26fd4dcd602681e";
+     String host = "http://192.168.0.106:8000";
+   String urlChecker = host+"/api/application_vc";
+     String jsonIP = "http://appchief.net/utils/get_real_ip.php";
+     String lib_ver = "1.0";
+
+}
